07fcb076eeaf08f70f54bd0dbe1c6351a02a2acf
Reza bolhasani <rblhx0@gmail.com>
2022-06-04 23:58:22 +0430

    initial

---
+++ sorting/sort.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "sort.h"
+
+static void swap(int* elements, int i , int j) {
+	int temp = elements[j];
+	elements[j] = elements[i];
+	elements[i] = temp;
+}
+
+void selection_sort(int* elements ,int size){
+	for (int i = 0 ; i < size ; i++){
+		int min = i;
+		for (int j = i+1; j < size ; j++) {
+			if (elements[j] < elements[min]) {
+				min = j;
+			}
+		} 
+		swap(elements,i,min);
+	}
+}
+
+void insertion_sort(int* elements, int size) {
+	for (int i = 1 ; i<size; i++) {
+		for (int j = i ; j > 0 && elements[j] < elements[j-1]; j-- ) {
+			swap(elements,j,j-1);
+		}
+	}
+}
+
+
+void print(int* elements,int size) {
+	printf("[ ");
+	for (int i = 0 ; i < size; i++) {
+		printf("%d ",elements[i]);
+	}
+	printf("]\n");
+}
---
+++ sorting/sort.h
@@ -0,0 +1,10 @@
+#ifndef sort_h
+#define sort_h
+
+
+void selection_sort(int* ,int );
+void insertion_sort(int* , int );
+void print(int* elements ,int );
+
+
+#endif
